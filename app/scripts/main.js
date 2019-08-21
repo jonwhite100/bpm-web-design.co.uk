f7ff1c1264e3033ae9ede801fba98b90a128041b
jon white <jon.white@cwideonline.com>
2019-08-21 16:00:19 +0100

    updated the contact form, added google maps and updated latest roles section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,18 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+// Google Maps
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 51.713113, lng: -1.009161};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {
+      	zoom: 16, center: uluru
+      }
+   );
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
