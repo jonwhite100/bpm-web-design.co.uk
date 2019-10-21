08e63c848acf4335daddcd2fabdbe1ad7d2e6e4e
jonwhite100 <jon@bpm-web-design.co.uk>
2019-10-21 21:15:24 +0100

    added CV link and fontawesome icons. tidied up content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
 
 // Uncomment to enable Bootstrap tooltips
 // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
@@ -7,6 +7,21 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+// fullpage.js initialise
+jQuery(document).ready(function($) {
+    $('#fullpage').fullpage({
+        licenseKey: '5E154A8E-77574575-843193BC-36825F00',
+        anchors: ['Welcome', 'About', 'Recent', 'Previous', 'Contact'],
+        sectionsColor: ['transparent', '#ffca3a', '#f15156', '#1982c4', '#6a4c93'],
+        verticalCentered: true,
+        css3:false,
+        continuousVertical: false,
+        slidesNavigation: true,
+        touchSensitivity: 50,
+        responsiveWidth: 767
+    });
+});
 
 // Google Maps
 // Initialize and add the map
