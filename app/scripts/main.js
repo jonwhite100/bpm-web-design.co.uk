1d0b2ae81d29e1e16cc652e52b19d7e723958f73
jon white <jon.white@cwideonline.com>
2019-10-22 13:45:00 +0100

    updated CV and some images. Final amends

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,8 +8,8 @@
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
 
-// fullpage.js initialise
 jQuery(document).ready(function($) {
+    // fullpage.js initialise
     $('#fullpage').fullpage({
         licenseKey: '5E154A8E-77574575-843193BC-36825F00',
         anchors: ['Welcome', 'About', 'Recent', 'Previous', 'Contact'],
@@ -21,19 +21,39 @@ jQuery(document).ready(function($) {
         touchSensitivity: 50,
         responsiveWidth: 767
     });
+
+    $(document).on('click', '.enlarge', function(){
+        $.fn.fullpage.setAllowScrolling(false);
+    });
+
+    $(document).on('click', '.turnScrollOn', function(){
+        $.fn.fullpage.setAllowScrolling(true);
+    });
+
+    // ekko lightbox
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox({
+            alwaysShowClose: true,
+        });
+    });
 });
 
-// Google Maps
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 51.713113, lng: -1.009161};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {
-      	zoom: 16, center: uluru
-      }
-   );
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+    // Initialize and add the map
+    function initMap() {
+        // The location of Uluru
+        var uluru = {lat: 51.713113, lng: -1.009161};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+              zoom: 11,
+              center: uluru
+            }
+        );
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({
+            position: uluru, map: map
+        });
+    }
+
+
